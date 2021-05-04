e0c4f42f005d043836995908b80b24743d713101
Shad02w@Win10 <alvistse123@gmail.com>
2021-05-04 20:11:18 +0800

    (Refactor) Added New rules

--- src/config/baseline.ts
+++ src/config/baseline.ts
@@ -15,13 +15,14 @@ export const baseline: Linter.Config = {
         'no-shadow': 'warn',
         'spaced-comment': ['error', 'always'],
         'comma-dangle': ['warn', 'never'],
+        quote: ['warn', 'single', { allowTemplateLiterals: true }],
         eqeqeq: ['error', 'always', { null: 'ignore' }],
         semi: ['warn', 'never', { beforeStatementContinuationChars: 'always' }],
         'lines-between-class-members': ['error', 'always'],
         'padding-line-between-statements': [
             'warn',
             { blankLine: 'always', prev: '*', next: ['function', 'export'] },
-            { blankLine: 'always', prev: ['function', 'import', 'cjs-import'], next: '*' },
+            { blankLine: 'always', prev: ['function', 'cjs-import'], next: '*' },
             { blankLine: 'always', prev: 'function', next: '*' }
         ],
         // @typescript-eslint/eslint-plugin
@@ -29,7 +30,7 @@ export const baseline: Linter.Config = {
         // eslint-plugin-react
         'react/jsx-max-props-per-line': [1, { maximum: 2, when: 'always' }],
         // eslint-plugin-eslint-comments
-        'eslint-comments/no-unused-disable': 'error',
+        'eslint-comments/no-unused-disable': 2,
         'eslint-comments/require-description': [
             'error',
             {
@@ -37,9 +38,12 @@ export const baseline: Linter.Config = {
             }
         ],
         // eslint-plugin-import
-        'import/no-relative-parent-import': 'error',
         'import/prevent-default-import': 'error',
+        'import/no-relative-parent-imports': 'error',
         'import/no-useless-path-segments': 'error',
+        'import/exports-last': 'error',
+        'import/first': 'error',
+        'import/newline-after-import': ['error', { count: 1 }],
         // eslint-plugin-react-hooks
         'react-hooks/rules-of-hooks': 'error',
         'react-hooks/exhaustive-deps': 'warn'
